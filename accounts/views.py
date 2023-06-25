b83aa8eed15867bcc2504e8e34168cd8a1d6f718
M4ti06 <104864383+M4ti06@users.noreply.github.com>
2023-06-25 13:00:59 +0200

    Added-link-to-reg-and-logic-for-reg-form

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from accounts import forms
+from django.contrib.auth import authenticate, login
 
 
 class RegistrationView(View):
 
     def get(self,request):
         return render(request, template_name="registration.html", context={"form": forms.UserRegistrationForm()})
+
+    def post(self, request, *args, **kwargs):
+        form = forms.UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect("home")
+        return render(request, "registration.html", {"form": form})
