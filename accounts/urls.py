b83f21da8b1100a2a1f7c330f3999ad83261c87d
M4ti06 <104864383+M4ti06@users.noreply.github.com>
2023-06-25 11:44:36 +0200

    Form-without-logic-for-database

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView
+from accounts import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('registration/', views.RegistrationView.as_view(), name='registration')
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from accounts import forms
 
-# Create your views here.
+
+class RegistrationView(View):
+
+    def get(self,request):
+        return render(request, template_name="registration.html", context={"form": forms.UserRegistrationForm()})
