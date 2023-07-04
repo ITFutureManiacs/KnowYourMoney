4a73753872210de3dc5cdc059541ea4214998697
M4ti06 <104864383+M4ti06@users.noreply.github.com>
2023-07-04 23:47:58 +0200

    dont working at all

--- KnowYourMoney/settings.py
+++ KnowYourMoney/settings.py
@@ -132,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
+
+#AUTHENTICATION_BACKENDS = ['accounts.backends.EmailOrLoginUsernameAuthenticationBackend']
