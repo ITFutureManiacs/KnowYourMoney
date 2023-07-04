11028612bae7198b0b59b048f97efa046e82cd1a
M4ti06 <104864383+M4ti06@users.noreply.github.com>
2023-07-04 19:43:04 +0200

    Login_form_attached_from_branch_about_polish_forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,3 +9,16 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "password1", "password2", "email"]
+
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = UsernameField(label="Login:", widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': '', 'id': 'hello'}))
+    password = forms.CharField(label="Hasło:", widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': '',
+            'id': 'hi',
+        }))
