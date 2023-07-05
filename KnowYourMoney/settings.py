d1734e8c930752ae43fb8ae1eddecbab74721c01
M4ti06 <104864383+M4ti06@users.noreply.github.com>
2023-07-05 18:11:26 +0200

    settings_modified

--- KnowYourMoney/settings.py
+++ KnowYourMoney/settings.py
@@ -93,17 +93,29 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'KnowYourMoney.accounts.validators.MyUserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'KnowYourMoney.accounts.validators.MyMinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'KnowYourMoney.accounts.validators.MyCommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'KnowYourMoney.accounts.validators.MyNumericPasswordValidator',
     },
+# {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
 ]
 
 
