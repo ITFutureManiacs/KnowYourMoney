b01277c2950f15fc6316143342334d31e93959a2
M4ti06 <104864383+M4ti06@users.noreply.github.com>
2023-07-06 23:33:30 +0200

    Fully_usable_filter_form_for_currencies_in_balance_view

---
+++ budget_manager/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from budget_manager.models import Currency
+
+
+class CurrencyFilter(forms.ModelForm):
+    currency_filter = forms.ModelChoiceField(queryset=Currency.objects.all(), initial="Zmień walutę")
+
+    class Meta:
+        model = Currency
+        exclude = ["currency_code","name"]
