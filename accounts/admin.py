52479c4c8116e824d14729188dabd87125fc94e2
KowalskiIT <k.now224@gmail.com>
2023-07-11 15:44:50 +0200

    restored ability to change password by admin on customuser

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
+from __future__ import unicode_literals
+
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from accounts import models
 
-# Register your models here.
-admin.site.register(models.CustomUser)
+admin.site.register(models.CustomUser, UserAdmin)
